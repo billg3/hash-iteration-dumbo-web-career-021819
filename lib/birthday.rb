207131499b3a4c7d311bcfc6ca7bba8ea0c68c92
Bill G <gbilly3@gmail.com>
2019-02-11 17:43:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,16 +14,3 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{key}! You are now #{value} years old!"
   }
 end 
-
-def select_winner(passengers)
-  # add the code snippet here!
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-  end
-
-winner
-end
-
