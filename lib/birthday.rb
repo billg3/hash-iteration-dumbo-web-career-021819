8bf6b4806015bbf251ffe36982e1bb3d26d861e6
Bill G <gbilly3@gmail.com>
2019-02-11 17:42:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,4 +15,15 @@ def happy_birthday(birthday_kids)
   }
 end 
 
+def select_winner(passengers)
+  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+
+winner
+end
 
